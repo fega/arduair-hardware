ced0202a8949358daf8dc02549da27ba9a3f4bbb
Fabian Enrique Gutierrez <fega.hg@gmail.com>
2017-03-18 08:10:22 -0500

    FIX: QA test

--- component tests/Winsen_sensor_QA/Winsen_sensor_QA.ino
+++ component tests/Winsen_sensor_QA/Winsen_sensor_QA.ino
@@ -10,41 +10,41 @@ void setup(){
 }
 
 void loop(){
-  int result = winsenRead();
-  Serial.write(result);
+  float result = winsenRead();
+  Serial.println(result, DEC);
 }
 /**
  * Setup the Winsen sensor
  */
 void winsenBegin(){
-  Serial1.begin(9600);
+  Serial3.begin(9600);
   byte setConfig[] = {0xFF,0x01, 0x78, 0x04, 0x00, 0x00, 0x00, 0x00, 0x83};//Set 1 Q/A configuration
-  Serial1.write(setConfig,sizeof(setConfig)); //
+  Serial3.write(setConfig,sizeof(setConfig)); //
   delay(1000);// Wait for a response
-  while(Serial1.available()>0){ // Flush the response
-    byte c = Serial1.read();
+  while(Serial3.available()>0){ // Flush the response
+    byte c = Serial3.read();
   }
 }
 /**
  * Read the sensor
  * @return {float} sensor [C]
  */
-int winsenRead(){
+float winsenRead(){
   byte petition[] = {0xFF,0x01, 0x78, 0x03, 0x00, 0x00, 0x00, 0x00, 0x84};// Petition to get a single result
   byte measure[8]={0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};// Space for the response
   float ppm;
-  Serial1.write(petition,sizeof(petition));
+  Serial3.write(petition,sizeof(petition));
 
   delay(2000);
 
-  if (Serial1.available() > 0) {
-    Serial1.readBytes(measure,9);
+  if (Serial3.available() > 0) {
+    Serial3.readBytes(measure,9);
     if (measure[0]==0xff && measure[1]==0x78){
-      Serial1.readBytes(measure,9);
+      Serial3.readBytes(measure,9);
     }
+
     if (measure[0]==0xff && measure[1]==0x86){
       ppm = measure[2]*256+measure[3];// this formula depends of the sensor is in the dataSheet
-
     }else{
       ppm=-1;
     }
